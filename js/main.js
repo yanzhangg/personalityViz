1614220fd0763a6e6b600a3bb0cd3c9c54d6d1ee
Clara Kim <clarakmj@students.cs.ubc.ca>
2023-03-28 12:12:40 -0700

    template

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+/**
+ * Load data from CSV file asynchronously and render charts
+ */
+ d3.csv('TODO.csv').then(data => {
+
+    // Convert columns to numerical values
+    data.forEach(d => {
+      Object.keys(d).forEach(attr => {
+      });
+    });
+  
+
+  });
